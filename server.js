773a135445b34e7fb90d598c3be13f8b19a9e16c
Neide Urbina <neide.n.urbina@gmail.com>
2021-09-28 16:44:57 -0500

    Budgtr ALL success

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//STATIC
+app.use(express.static("public"));
+
+//INDEX
 app.get('/budgets', (req, res) => {
-    res.send('Hello World')
-})
+    res.render('index.ejs');
+});
+
+//NEW
+app.get('budgets/new', (req, res) => {
+    res.send('new success');
+});
+
+//CREATE
+app.post('/budgets', (req, res) => {
+    res.send('create success');
+});
+
+//SHOW
+app.get('/budgets/:index', (req, res) => {
+    res.render('show.ejs');
+});
 
 app.listen(PORT, () => {
     console.log(`Listening ${PORT}`);
